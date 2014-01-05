b3f63fb2c1cf5f56e7658d99ebb8e6e9bb217b1e
Krzysztof Bury <Krzysztof.bury@me.com>
2014-01-05 11:10:39 +0100

    PROJEKT 1

---
+++ HelloOpenGLES20/src/com/example/android/opengl/Piramid.java
@@ -0,0 +1,106 @@
+package com.example.android.opengl;
+
+import android.util.Log;
+
+/**
+ * Created by krzysztofbury on 05.01.2014.
+ */
+public class Piramid {
+    static float[] piramidCoords = {
+            // in counterclockwise order:
+            0.0f, 0.622008459f, 0.0f,   // top 0
+            -0.5f, -0.311004243f, 0.5f,  // front left 3
+            0.5f, -0.311004243f, 0.5f,    // front right 6
+            - 0.5f, -0.311004243f, -0.5f,  // back left  9
+            0.5f, -0.311004243f, -0.5f    // back right 12
+    };
+
+    Triangle front;
+    Triangle left;
+    Triangle right;
+    Triangle back;
+    Triangle bottom1;
+    Triangle bottom2;
+
+
+    public Piramid() {
+
+
+        float frontCoords[] = {
+                piramidCoords[0],piramidCoords[1],piramidCoords[2],
+                piramidCoords[6],piramidCoords[7],piramidCoords[8],
+                piramidCoords[3],piramidCoords[4],piramidCoords[5]
+        };
+        front = new Triangle(frontCoords);
+        float colFront[] = {1.0f,0f,0f,1f};
+        front.setColor(colFront);
+
+        float leftCoords[] = {
+                piramidCoords[0],piramidCoords[1],piramidCoords[2],
+                piramidCoords[3],piramidCoords[4],piramidCoords[5],
+                piramidCoords[9],piramidCoords[10],piramidCoords[11]
+        };
+        left = new Triangle(leftCoords);
+        float colleft[] = {0.0f,1f,0f,1f};
+        left.setColor(colleft);
+
+
+        float rightCoords[] = {
+                piramidCoords[0],piramidCoords[1],piramidCoords[2],
+                piramidCoords[12],piramidCoords[13],piramidCoords[14],
+                piramidCoords[6],piramidCoords[7],piramidCoords[8]
+        };
+
+        right = new Triangle(rightCoords);
+        float colright[] = {0.0f,0f,1.f,1f};
+        right.setColor(colright);
+
+
+        float backCoords[] = {
+                piramidCoords[0],piramidCoords[1],piramidCoords[2],
+                piramidCoords[9],piramidCoords[10],piramidCoords[11],
+                piramidCoords[12],piramidCoords[13],piramidCoords[14]
+        };
+        back = new Triangle(backCoords);
+        float colBack[] = {1.0f,0f,1.0f,1f};
+        back.setColor(colBack);
+
+
+        float bottom1Coords[] = {
+                piramidCoords[3],piramidCoords[4],piramidCoords[5],
+                piramidCoords[6],piramidCoords[7],piramidCoords[8],
+                piramidCoords[9],piramidCoords[10],piramidCoords[11]
+        };
+        bottom1 = new Triangle(bottom1Coords);
+        float colbottom1[] = {1.0f,1.0f,1.0f,1f};
+        bottom1.setColor(colbottom1);
+
+        float bottom2Coords[] = {
+                piramidCoords[6],piramidCoords[7],piramidCoords[8],
+                piramidCoords[12],piramidCoords[13],piramidCoords[14],
+                piramidCoords[9],piramidCoords[10],piramidCoords[11]
+        };
+        bottom2 = new Triangle(bottom2Coords);
+        float colbottom2[] = {0f,1f,1.0f,1f};
+        bottom2.setColor(colbottom2);
+    }
+
+
+
+
+
+
+
+
+
+    public void draw(float[] mvpMatrix) {
+        front.draw(mvpMatrix);
+        left.draw(mvpMatrix);
+        right.draw(mvpMatrix);
+        back.draw(mvpMatrix);
+        bottom1.draw(mvpMatrix);
+        bottom2.draw(mvpMatrix);
+    }
+
+
+}
