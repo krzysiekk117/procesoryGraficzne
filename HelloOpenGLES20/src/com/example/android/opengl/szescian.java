f880fc814e985ee336c77f25cab1176245ec898e
MagdaBednarska <magdalenabednarskaaaa@gmail.com>
2014-01-24 11:13:13 +0100

    no message

--- HelloOpenGLES20/src/com/example/android/opengl/szescian.java
+++ HelloOpenGLES20/src/com/example/android/opengl/szescian.java
@@ -21,54 +21,96 @@ public class szescian {
 
 
         };
-    // zmiana madzi
+
 
         Square front;
         Square left;
         Square right;
         Square back;
+        Square bottom1;
+        Square bottom2;
 
         public szescian() {
 
 
         float frontCoords[] = {
-                szescianCoords[0],szescianCoords[1],szescianCoords[2],
-                szescianCoords[3],szescianCoords[4],szescianCoords[5],
-                szescianCoords[6],szescianCoords[7],szescianCoords[8],
+                szescianCoords[21],szescianCoords[22],szescianCoords[23],
                 szescianCoords[9],szescianCoords[10],szescianCoords[11],
+                szescianCoords[0],szescianCoords[1],szescianCoords[2],
+                szescianCoords[12],szescianCoords[13],szescianCoords[14],
             };
 
-            front = new Square(frontCoords);
+            front = new Square(frontCoords);  //czerwony
             float colFront[] = {1.0f,0f,0f,1f};
             front.setColor(colFront);
 
 
             float rightCoords[] = {
-                    szescianCoords[21],szescianCoords[22],szescianCoords[23],
                     szescianCoords[18],szescianCoords[19],szescianCoords[20],
+                    szescianCoords[6],szescianCoords[7],szescianCoords[8],
                     szescianCoords[3],szescianCoords[4],szescianCoords[5],
-                    szescianCoords[0],szescianCoords[1],szescianCoords[2],
+                    szescianCoords[15],szescianCoords[16],szescianCoords[17],
             };
 
-            right = new Square(rightCoords);
+            right = new Square(rightCoords); //zielony
             float colRight[] = {0.0f,1f,0f,1f};
             right.setColor(colRight);
 
             float backCoords[] = {
-                    szescianCoords[12],szescianCoords[13],szescianCoords[14],
-                    szescianCoords[15],szescianCoords[16],szescianCoords[17],
-                    szescianCoords[18],szescianCoords[19],szescianCoords[20],
-                    szescianCoords[21],szescianCoords[22],szescianCoords[23],
+                    szescianCoords[9],szescianCoords[10],szescianCoords[11],
+                    szescianCoords[6],szescianCoords[7],szescianCoords[8],
+                    szescianCoords[3],szescianCoords[4],szescianCoords[5],
+                    szescianCoords[0],szescianCoords[1],szescianCoords[2],
 
             };
 
 
-            back = new Square(backCoords);
-            float colBack[] = {0.0f,0f,1f,1f};
+            back = new Square(backCoords); //niebeski
+            float colBack[] = {0.0f,0.0f,1.0f,1.0f};
             back.setColor(colBack);
 
 
+            float leftCoords[] = {
+                   szescianCoords[0],szescianCoords[1],szescianCoords[2],
+                   szescianCoords[3],szescianCoords[4],szescianCoords[5],
+                   szescianCoords[15],szescianCoords[16],szescianCoords[17],
+                   szescianCoords[12],szescianCoords[13],szescianCoords[14],
+
+            };
+
+            left = new Square(leftCoords);//zolty
+            float colLeft[] = {1.0f,1.0f,0.0f,1.0f};
+            left.setColor(colLeft);
+
+
+
+            float bottom1Coords[] = {
+                    szescianCoords[21],szescianCoords[22],szescianCoords[23],
+                    szescianCoords[18],szescianCoords[19],szescianCoords[20],
+                    szescianCoords[6],szescianCoords[7],szescianCoords[8],
+                    szescianCoords[9],szescianCoords[10],szescianCoords[11],
+            };
+
+            bottom1 = new Square(bottom1Coords);  //fioletowy
+            float colBottom1[] = {1.0f,0.0f,1.0f,1.0f};
+            bottom1.setColor(colBottom1);
+
+
+            float bottom2Coords[] = {
+                    szescianCoords[21],szescianCoords[22],szescianCoords[23],
+                    szescianCoords[18],szescianCoords[19],szescianCoords[20],
+                    szescianCoords[15],szescianCoords[16],szescianCoords[17],
+                    szescianCoords[12],szescianCoords[13],szescianCoords[14],
+            };
+
+            bottom2 = new Square(bottom2Coords);  //biały
+            float colBottom2[] = {1.0f,1.0f,1.0f,1.0f};
+            bottom2.setColor(colBottom2);
+
         }
+
+
+
 
 
 
